184f49f32e6323153eff3450a4e96f8641da579d
xarn <arnaud.dagnelies@gmail.com>
2016-05-26 19:43:20 +0000

    ...

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'starpath',
   packages=['starpath'],
-  version = '1.0.1',
+  version = '1.0.2',
   description = 'JSON Pointers with wildcards!',
   author = 'Arnaud Dagnelies',
   author_email = 'arnaud.dagnelies@gmail.com',
